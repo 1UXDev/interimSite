1af6c9f75cc735413db0d1825e04a675c962a453
mntzd <julien.leske@gmail.com>
2023-08-21 14:53:37 +0200

    basic site, content optimized

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,64 +40,102 @@ export default function Home() {
           </div>
         </section>
         <section className={`${styles.section}`}>
-          <h2>Challenge to Concept</h2>
-          <img alt="In 6 weeks to a validated and interactive Prototype through agile methodology"></img>
+          <h2>From Challenge to Concept in 2 weeks</h2>
           <article>
             <h3>Research</h3>
             <p>
-              User & Stakeholder Interviews, Quantitative Data Analysis, Market
-              Research, Experience Analysis of status quo Products & Services
+              Quantify Market Opportunities through Qualitative and Quantitative
+              Research to target the needs of relevant user segments
             </p>
+            <span>User & Stakeholder Interviews</span>{" "}
+            <span>Quantitative Data Analysis</span>{" "}
+            <span>Market & Competitor Research</span>{" "}
+            <span>Experience Analysis</span>
           </article>
           <article>
             <h3>Ideate</h3>
-            <p>Design Thinking, Google Sprint & Lean UX Facilitation</p>
+            <p>
+              Get your stakeholders on board and find creative solutions to a
+              common problem understandingä
+            </p>
+            <span>Design Thinking</span>
+            <span>Google Sprint</span>
+            <span>Lean UX</span>
           </article>
           <article>
             <h3>Align</h3>
             <p>
-              Stakeholderfeedback, Concept Creation, Personas, Customer Journeys
-              & Service Blueprints
+              Create a detailed concept describing Challenge, Quantified Market
+              Opportunity and Solutionscenarios to align with important
+              stakeholders
             </p>
+            <span>Alignment Workshop</span>{" "}
+            <span>Personas & Customer Journeys</span>{" "}
+            <span>Service Blueprint</span>
           </article>
         </section>
 
         <section className={`${styles.section}`}>
-          <h2>Concept to Prototype</h2>
+          <h2>Concept to Prototype in 4 weeks</h2>
           <article>
             <h3>Prototype</h3>
-            <p>Wireframing, Prototyping, MVPs</p>
+            <p>
+              Create interactive proof of concepts in a few days to excite
+              stakeholders for your ideas, A/B Test and gather market feedback
+              efficiently
+            </p>
+            <span>Wireframing</span>
+            <span>Prototyping</span>
+            <span>MVPs</span>
           </article>
           <article>
             <h3>Test & Iterate</h3>
-            <p>Scaled User Research, User & Usability Testing</p>
+            <p>
+              Validate & prioritize solution ideas and their features to
+              coordinate time and ressource investments where value creation is
+              maximized
+            </p>
+            <span>Scaled User Research</span>
+            <span>Usability Testing</span>
+            <span>A/B Testing</span>
           </article>
           <article>
             <h3>Plan</h3>
             <p>
-              Backlog Creation, Requirements Engineering, Planning Meeting with
-              Stakeholders
+              Ramp-up the development process or hand-over to your
+              development-team. Slice solution into releases
             </p>
+            <span>Requirement Engineering</span>
+            <span>Backlog Planning</span>
+            <span>Alignment with Stakeholders</span>
           </article>
         </section>
 
         <section className={`${styles.section}`}>
-          <h2>Prototype to Product</h2>
+          <h2>From Prototype to Product</h2>
           <article>
             <h3>Build</h3>
             <p>
-              Programming, Design Systems, Usability Norms, OZG & BFSG
-              compliance, React, Mongo, Express, Node, HTML, CSS, JS, Git,
-              Bootstrap, Tailwind, Python, Webscraping
+              Bring Prototypes into Production, also possible as Consulting
+              Service for your Development Team
             </p>
+            <span>Full-Stack Webdevelopment</span>
+            <span>Design Systems</span>
+            <span>Compliant (OZG, BfsG, 9241-110, A11y)</span>
           </article>
           <article>
             <h3>Roll-Out</h3>
-            <p>...</p>
+            <p>Continuously deliver value to your user-segments</p>
+            <span>Localized Roll-Out Strategy</span>
+            <span>Performance Monitoring</span>
+            <span>Bugfixes</span>
           </article>
           <article>
             <h3>Operate & Manage</h3>
-            <p>Continuous Performance Analysis, Updates & Improvements</p>
+            <p></p>
+            <span></span>
+            <span></span>
+            <span></span>
           </article>
         </section>
 
